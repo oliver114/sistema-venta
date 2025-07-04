e44309144d9b61a6f43aa83450e0d63544ac4ba2
OLIVERCHA <979307965isa@gmail.com>
2025-07-04 14:42:32 -0500

    Actualizar configuración de base de datos

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,13 @@
+// 
 // backend/db.js
 const mariadb = require('mariadb');
 
 const pool = mariadb.createPool({
-  host: 'localhost',
-  user: 'User_system',
-  password: '44763562',
-  database: 'BDventas',
-  port: 3307,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
   connectionLimit: 5
 });
 
